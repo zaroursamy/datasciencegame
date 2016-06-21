d1adf45c8c353167429ca15d169c0d9a79a00351
zaroursamy <samy.zarour8430@gmail.com>
2016-06-22 00:36:10 +0200

    continue sur dictionnary learning: transofmration en gris

--- code/dictionnaryLearning.py
+++ code/dictionnaryLearning.py
@@ -20,13 +20,18 @@ from sklearn.utils.fixes import sp_version
 
 # 
 img = Image.open("roof_images\\-1191173.jpg")
+img = ImageOps.grayscale(img)
 img = np.asarray(img)
+img = img.astype(np.float)
 patch_size = (10,10)
 data = extract_patches_2d(img, patch_size)
+data = data.reshape(data.shape[0], -1)
+data -= np.mean(data, axis=0)
+data /= np.std(data, axis=0)
 
 # il faut n_components > ncol des images 
 # initialisation d'un dictionnaire
 # n_components: taille du dictionnaire
 dico = MiniBatchDictionaryLearning(n_components=100, alpha=1, n_iter=500)
 
-V = dico.fit(data)
+V = dico.fit(data).components_
