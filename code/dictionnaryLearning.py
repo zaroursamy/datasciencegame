cbc5e93534cb91dd84a97444232b6266c50ce107
zaroursamy <samy.zarour8430@gmail.com>
2016-06-23 13:48:32 +0200

    ajout des temps dexecution

--- code/dictionnaryLearning.py
+++ code/dictionnaryLearning.py
@@ -9,19 +9,20 @@ import numpy as np
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+from time import time
 
 #• définition de la taille des blocs (patchs)
 
 
 # prend une image en entrée et renvoie un tableau de niveau de gris
 img = Image.open("roof_images\\38128663.jpg")
+
 def imgToGrayArray(img, resize=False, shape=img.size):
     if resize:
         img = img.resize(shape, 0)
 
     img = ImageOps.grayscale(img)
-    img2 = np.asarray(img)
-    img2 = img2.astype(np.float)
+    img2 = np.asarray(img).astype(np.float)
     
     return(img2)
 
@@ -41,14 +42,22 @@ def constructPatches(img2, patch_size, scale=True):
     
     return(data)
 
+print('extraction des patches')
+t0 = time()
 data = constructPatches(img2, patch_size)
-
+t1 = time() - t0
+print('temps d\'extraction: %.2fs' % t1)
 # il faut n_components > ncol des images 
 # initialisation d'un dictionnaire
 # n_components: taille du dictionnaire
 # on fit le dictionnaire sur l'image de base normalisée
-dico = MiniBatchDictionaryLearning(n_components=5*img2.shape, alpha=1, n_iter=100, n_jobs=-1)#, transform_algorithm='omp')
+
+print('construction du dictionnaire et fit sur les data')
+t0 = time()
+dico = MiniBatchDictionaryLearning(n_components=2*img2.shape[1], alpha=1, n_iter=100, n_jobs=-1)#, transform_algorithm='omp')
 V = dico.fit(data).components_
+t1 = t0 - time()
+print('temps fit dico: %.fs ' % t1)
 
 """
 # on enleve le bruit que l'on suppose gaussien
@@ -59,7 +68,7 @@ data -= intercept
 """
 
 # définition des algos de transformations (OMP avec 1 et 2 atomes, LAR regression 5 atomes, et autre chose )
-transform_algorithms = [('omp1', 'omp',{'transform_n_nonzero_coefs': 1}),('omp2', 'omp',{'transform_n_nonzero_coefs': 2})]
+transform_algorithms = [('omp1', 'omp',{'transform_n_nonzero_coefs': 10})]
 """
 ,
 ('Least-angle regression\n5 atoms', 'lars',
@@ -68,17 +77,25 @@ transform_algorithms = [('omp1', 'omp',{'transform_n_nonzero_coefs': 1}),('omp2'
 """
     
 
-#} reconstruction = image reconstruite
+#} création de plusieurs images reconstruites stockées dans un dictionnaire
 reconstructions = {}
 for title, transform_algorithm, kwargs in transform_algorithms:
     reconstructions[title] = img2.copy()
     dico.set_params(transform_algorithm=transform_algorithm, **kwargs)
+    print('calcul de la transformation des data dans le dico')
+    t0 = time()
     code = dico.transform(data)
+    t1 = t0 - time()
+    print('temps transformation data dans dico: %.fs' % t1)
     patches = np.dot(code, V)
     patches = patches.reshape(len(data), *patch_size)
     # on enleve les img.size[1]//2 premieres colonnes 
     #reconstructions[title][:, img.size[1]//2:] = reconstruct_from_patches_2d(patches, (img.size[0], img.size[1]//2))
+    print('reconstruction de l\'image par les patchs')
+    t0 = time()    
     reconstructions[title] = reconstruct_from_patches_2d(patches, img2.shape)
+    t1 = t0 - time()
+    print('temps reconstruction par patches: %.fs' % t1)
 
     """if transform_algorithm == 'threshold':
         patches -= patches.min()
